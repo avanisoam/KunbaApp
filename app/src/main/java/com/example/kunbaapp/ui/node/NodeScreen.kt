5244c4be2656501d3a7322900db00739d473a499
Avani Tanwar <avanisoam@gmail.com>
2024-07-26 07:51:16 +0200

    Dialog poc

--- app/src/main/java/com/example/kunbaapp/ui/node/NodeScreen.kt
+++ app/src/main/java/com/example/kunbaapp/ui/node/NodeScreen.kt
@@ -1,12 +1,19 @@
 package com.example.kunbaapp.ui.node
 
+import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Create
+import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -15,14 +22,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import com.example.kunbaapp.R
 import com.example.kunbaapp.ui.family.FamilyDestination
 import com.example.kunbaapp.ui.navigation.NavigationDestination
+import com.example.kunbaapp.ui.shared.CustomFloatingButton
+import com.example.kunbaapp.ui.shared.FABItem
 import com.example.kunbaapp.ui.shared.KunbaAppTopBar
 import com.example.kunbaapp.ui.shared.NodeItem
+import com.example.kunbaapp.ui.shared.PopupDialog
 import org.koin.androidx.compose.getViewModel
 
 object NodeDestination : NavigationDestination {
@@ -43,6 +57,7 @@ fun NodeScreen(
     val uiState by viewModel.uiState.collectAsState()
 
     //Text(text = uiState.node.toString())
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             KunbaAppTopBar(
@@ -55,6 +70,52 @@ fun NodeScreen(
             )
         },
         floatingActionButton = {
+            val itemList = listOf(
+                FABItem(icon = Icons.Rounded.Home, text = "Home",uniqueId="open_home"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add father",uniqueId="add_father"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add mother",uniqueId="add_mother"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add son",uniqueId="add_son"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add daughter",uniqueId="add_daughter"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add brother",uniqueId="add_brother"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add sister",uniqueId="add_sister"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add spouse",uniqueId="add_spouse"),
+                FABItem(icon = Icons.Rounded.Add, text = "Add half-sibling",uniqueId="add_half_sibling"),
+            )
+            val openAlertDialog = remember { mutableStateOf(false) }
+            if(openAlertDialog.value)
+            {
+                PopupDialog(
+                    onDismissRequest = { openAlertDialog.value = false },
+                    onConfirmation = {  openAlertDialog.value = false
+                        println("Confirmation registered") // Add logic here to handle confirmation. },
+                    },
+                    dialogTitle = "Alert dialog example",
+                    dialogText = "This is an example of an alert dialog with buttons.",
+                    icon = Icons.Rounded.AccountCircle,
+                )
+            }
+            CustomFloatingButton(
+                items = itemList,
+                onItemClick = {item ->
+
+                    when(item.uniqueId) {
+                        "open_home" ->  {
+                            // navigating to Home
+                            navigateToHome()
+                        }//Toast.makeText(context, "call clicked", Toast.LENGTH_SHORT).show()
+                        "add_father" -> openAlertDialog.value=true//Toast.makeText(context, "create clicked", Toast.LENGTH_SHORT).show()
+                        "add_mother" -> Toast.makeText(context, "account clicked", Toast.LENGTH_SHORT).show()
+                        "add_son" -> Toast.makeText(context, "account clicked", Toast.LENGTH_SHORT).show()
+                        "add_daughter" -> Toast.makeText(context, "account clicked", Toast.LENGTH_SHORT).show()
+                        "add_brother" -> Toast.makeText(context, "account clicked", Toast.LENGTH_SHORT).show()
+                        "add_sister" -> Toast.makeText(context, "account clicked", Toast.LENGTH_SHORT).show()
+                        "add_spouse" -> Toast.makeText(context, "account clicked", Toast.LENGTH_SHORT).show()
+                        "add_half_sibling" -> Toast.makeText(context, "account clicked", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            )
+            /*
             FloatingActionButton(
                 onClick = navigateToHome,
                 shape = MaterialTheme.shapes.medium,
@@ -62,10 +123,11 @@ fun NodeScreen(
             ) {
                 Icon(
                     //imageVector = if(uiState.isFavorite) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
-                    imageVector = Icons.Filled.Home,
+                    imageVector = Icons.Filled.Add,
                     contentDescription = stringResource(R.string.go_to_favorite)
                 )
             }
+             */
         }
     ) {innerPadding ->
         LazyColumn(modifier = Modifier.padding(innerPadding)) {
---
+++ app/src/main/java/com/example/kunbaapp/ui/shared/PopupDialog.kt
@@ -0,0 +1,71 @@
+package com.example.kunbaapp.ui.shared
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun PopupDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            //Text(text = dialogText)
+            AddNode()
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
+
+@Composable
+fun AddNode() {
+    var medicationName by rememberSaveable { mutableStateOf("") }
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = medicationName,
+        onValueChange = { medicationName = it },
+        placeholder = { Text(text = "e.g. Hexamine") },
+    )
+}
